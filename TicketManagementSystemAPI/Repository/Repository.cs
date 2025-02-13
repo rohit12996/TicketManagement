936c62c7bca0718e8d0b535f778d5f3ce5000917
Rohit <rajr09165@gmail.com>
2025-02-13 23:03:56 +0530

    minor changes

--- TicketManagementSystemAPI/Repository/Repository.cs
+++ TicketManagementSystemAPI/Repository/Repository.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TicketManagementSystemAPI.Models;
 using TicketManagementSystemAPI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class Repository<T> : IRepository<T> where T : class
 {
@@ -15,24 +18,61 @@ public class Repository<T> : IRepository<T> where T : class
 
     public async Task<T> GetByIdAsync(int id)
     {
-        return await _dbSet.FindAsync(id);
+        try
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        catch (Exception ex)
+        {
+            // Log the exception (you might want to use a logging framework here)
+            throw new ApplicationException($"Unable to retrieve entity with ID {id}.", ex);
+        }
     }
 
     public async Task<IEnumerable<T>> GetAllAsync()
     {
-        return await _dbSet.ToListAsync();
+        try
+        {
+            return await _dbSet.ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            // Log the exception
+            throw new ApplicationException("Unable to retrieve entities.", ex);
+        }
     }
 
     public async Task AddAsync(T entity)
     {
-        await _dbSet.AddAsync(entity);
-        await _context.SaveChangesAsync();
+        try
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex)
+        {
+            // Log the exception
+            throw new ApplicationException("Unable to add entity.", ex);
+        }
     }
 
     public async Task UpdateAsync(T entity)
     {
-        _dbSet.Attach(entity);
-        _context.Entry(entity).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
+        try
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            // Log the exception
+            throw new ApplicationException("Unable to update entity due to concurrency issues.", ex);
+        }
+        catch (DbUpdateException ex)
+        {
+            // Log the exception
+            throw new ApplicationException("Unable to update entity.", ex);
+        }
     }
 }
